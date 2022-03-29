d6743b6702418c5f53caf39d648e616df40a5553
Serafym Zolotukhin <serafymz@nayax.com>
2022-03-29 12:07:51 +0300

    Added vars.

--- src/main/java/com/addresstest/validators/AddressDtoValidatorImpl.java
+++ src/main/java/com/addresstest/validators/AddressDtoValidatorImpl.java
@@ -7,12 +7,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class AddressDtoValidatorImpl implements AddressDtoValidator{
 
+    private static final int MAX_VARCHAR_LENGTH = 50;
+
     @Override
     public void validate(AddressDto addressDto) {
         if (addressDto == null) {
             throw new NullPointerException("Address dto is null.");
         }
-        if (addressDto.getAddress().length() > 50) {
+        if (addressDto.getAddress().length() > MAX_VARCHAR_LENGTH) {
             throw new MaxStringFieldValueExceedException("Exceeded maximum value for string field address. " +
                      " Value: " + addressDto.getAddress() + ". " +
                      "Value length = " +addressDto.getAddress().length());

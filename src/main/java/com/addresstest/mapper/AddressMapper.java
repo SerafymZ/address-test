e30134f8c205499d1d37d70f827d5d38cac9fff4
Serafym Zolotukhin <serafymz@nayax.com>
2022-04-07 10:48:16 +0300

    Used mapstruct for address mapper.

--- src/main/java/com/addresstest/mapper/AddressMapper.java
+++ src/main/java/com/addresstest/mapper/AddressMapper.java
@@ -2,7 +2,13 @@ package com.addresstest.mapper;
 
 import com.addresstest.dto.AddressDto;
 import com.addresstest.entity.AddressEntity;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.ERROR,
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface AddressMapper {
     AddressEntity toEntity(AddressDto addressDto);
     AddressDto toDto(AddressEntity addressEntity);

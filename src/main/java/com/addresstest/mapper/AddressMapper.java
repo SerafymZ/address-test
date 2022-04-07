918aaed5c92a6f6bef2916b900b2f76c05e311dc
Serafym Zolotukhin <serafymz@nayax.com>
2022-04-07 15:07:31 +0300

    Fixed mapstruct.

--- src/main/java/com/addresstest/mapper/AddressMapper.java
+++ src/main/java/com/addresstest/mapper/AddressMapper.java
@@ -2,14 +2,14 @@ package com.addresstest.mapper;
 
 import com.addresstest.dto.AddressDto;
 import com.addresstest.entity.AddressEntity;
-import org.mapstruct.InjectionStrategy;
-import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.ERROR,
-        injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
 public interface AddressMapper {
     AddressEntity toEntity(AddressDto addressDto);
+
     AddressDto toDto(AddressEntity addressEntity);
 }
